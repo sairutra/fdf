4388d69d7f0910bb5bfc39b677d5aebfa5475cef
spenning <whimizera@mailbox.org>
2024-05-20 16:43:33 +0200

    parse_colom norm proof

--- src/parse/parse_colom.c
+++ src/parse/parse_colom.c
@@ -6,29 +6,39 @@
 /*   By: spenning <spenning@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 14:53:56 by spenning          #+#    #+#             */
-/*   Updated: 2024/05/12 14:54:10 by spenning         ###   ########.fr       */
+/*   Updated: 2024/05/20 16:42:52 by spenning         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/fdf.h"
 
-int parse_colom(char *path)
+char	**parse_colom_split(int fd)
+{
+	char	*buf;
+	char	**splitbuf;
+
+	if (fd == -1)
+		return (NULL);
+	buf = get_next_line(fd, 0);
+	close(fd);
+	if (buf == NULL)
+		return (NULL);
+	splitbuf = ft_split(buf, ' ');
+	free(buf);
+	if (splitbuf == NULL)
+		return (NULL);
+	return (splitbuf);
+}
+
+int	parse_colom(char *path)
 {
 	int		count;
 	int		fd;
-	char	*buf;
 	char	**splitbuf;
 
 	count = 0;
 	fd = open(path, O_RDONLY);
-	if (fd == -1)
-		return (-1);
-	buf = get_next_line(fd, 0);
-	close(fd);
-	if (buf == NULL)
-		return (-1);
-	splitbuf = ft_split(buf, ' ');
-	free(buf);
+	splitbuf = parse_colom_split(fd);
 	if (splitbuf == NULL)
 		return (-1);
 	while (splitbuf[count] != NULL)
@@ -36,7 +46,7 @@ int parse_colom(char *path)
 		if (ft_strncmp(splitbuf[count], " ", 1))
 			count++;
 	}
-	if(!ft_strncmp(splitbuf[count - 1], "\n", 1))
+	if (!ft_strncmp(splitbuf[count - 1], "\n", 1))
 		count--;
 	free_char_array(splitbuf);
 	return (count);
